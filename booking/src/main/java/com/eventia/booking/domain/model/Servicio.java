9bef757f814903e637ef77d463246e5267849807
pasoto0 <nparedess@ucundinamarca.edu.co>
2025-11-19 15:25:58 -0500

    Declaración atributos clase Servicio en microservicio Booking

--- booking/src/main/java/com/eventia/booking/domain/model/Servicio.java
+++ booking/src/main/java/com/eventia/booking/domain/model/Servicio.java
@@ -1,4 +1,23 @@
 package com.eventia.booking.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+
+@AllArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
+
 public class Servicio {
+    private Long Id_Servicio;
+    private Long Id_Usuario_Oferente;
+    private String Descripcion;
+    private Double Costo;
+    private String Tipo_Servicio;
+    private String Ciudad;
 }

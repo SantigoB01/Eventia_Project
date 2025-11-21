40f93d3a3e860ddfc6a0fa998bb87280494aa917
Alison <alimoon1306@gmail.com>
2025-11-21 00:39:48 -0500

    Se agrega contenido en las clases

--- notifications/src/main/java/com/eventia/notifications/infraestructure/sqs/SqsListener.java
+++ notifications/src/main/java/com/eventia/notifications/infraestructure/sqs/SqsListener.java
@@ -1,4 +1,59 @@
 package com.eventia.notifications.infraestructure.sqs;
 
+import com.eventia.notifications.infraestructure.ses.SesEmailSender;
+import com.eventia.notifications.infraestructure.sns.SnsSmsSender;
+import com.eventia.notifications.infraestructure.sqs.dto.EventoNotificacionDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+
+import java.util.List;
+import java.util.concurrent.Executors;
+
+@Component
+@RequiredArgsConstructor
 public class SqsListener {
+
+    private final SqsClient sqsClient;
+    private final ObjectMapper objectMapper;
+    private final SnsSmsSender snsSmsSender;
+    private final SesEmailSender sesEmailSender;
+
+    private final String QUEUE_URL = "";
+
+    @PostConstruct
+    public void escucharMensaje(){
+        Executors.newSingleThreadExecutor().submit(() -> {
+            while (true) {
+                ReceiveMessageRequest receiveRequest = ReceiveMessageRequest.builder()
+                        .queueUrl(QUEUE_URL)
+                        .maxNumberOfMessages(5)
+                        .waitTimeSeconds(10)
+                        .build();
+
+                List<Message> messages = sqsClient.receiveMessage(receiveRequest).messages();
+
+                for (Message message : messages) {
+                    try {
+                        EventoNotificacionDTO evento = objectMapper.readValue(message.body(), EventoNotificacionDTO.class);
+
+                        snsSmsSender.enviarSms(evento.getMensaje(), evento.getNumeroTelefono());
+                        sesEmailSender.enviarEmail(evento.getEmail(), evento.getTipo(), evento.getMensaje());
+
+                        sqsClient.deleteMessage(DeleteMessageRequest.builder()
+                                .queueUrl(QUEUE_URL)
+                                .receiptHandle(message.receiptHandle())
+                                .build());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
 }
